af00a9c5364c45df96b86991648c2b02f70e0515
Ryan Dougherty <1001rmd@gmail.com>
2019-03-25 21:10:45 -0400

    Fixed Issue with Example Comments not compiling

--- app/controllers/CountController.java
+++ app/controllers/CountController.java
@@ -10,7 +10,7 @@ import javax.inject.Singleton;
 /**
  * This controller demonstrates how to use dependency injection to
  * bind a component into a controller class. The class contains an
- * action that shows an incrementing count to users. The {@link Counter}
+ * action that shows an incrementing count to users. The {link Counter}
  * object is injected by the Guice dependency injection system.
  */
 @Singleton
@@ -24,7 +24,7 @@ public class CountController extends Controller {
     }
 
     /**
-     * An action that responds with the {@link Counter}'s current
+     * An action that responds with the {link Counter}'s current
      * count. The result is plain text. This action is mapped to
      * <code>GET</code> requests with a path of <code>/count</code>
      * requests by an entry in the <code>routes</code> config file.
--- app/services/AtomicCounter.java
+++ app/services/AtomicCounter.java
@@ -3,12 +3,13 @@ package services;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.*;
 
+
 /**
  * This class is a concrete implementation of the {@link Counter} trait.
  * It is configured for Guice dependency injection in the {@link Module}
  * class.
  *
- * This class has a {@link Singleton} annotation because we need to make
+ * This class has a {link Singleton} annotation because we need to make
  * sure we only use one counter per application. Without this
  * annotation we would get a new instance every time a {@link Counter} is
  * injected.
