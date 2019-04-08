c0d0aa01b7ed8ad9c7adee2f60eb9bb4e73e32a8
AndrewWitherite <47163001+AndrewWitherite@users.noreply.github.com>
2019-04-07 23:46:07 -0400

    Update StockController.java

--- app/controllers/StockController.java
+++ app/controllers/StockController.java
@@ -11,10 +11,18 @@ import play.data.FormFactory;
 import play.i18n.MessagesApi;
 import play.mvc.*;
 
+import org.json.JSONObject;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.math.BigDecimal;
+import play.libs.Json.*;
 
 import static play.libs.Scala.asScala;
 
@@ -22,6 +30,7 @@ import static play.libs.Scala.asScala;
 public class StockController extends Controller{
 
     private final Form<Stock> form;
+    //public Form <listStock> forms;
     private MessagesApi messagesApi;
     private final ArrayList<Stock> stocks;
 
@@ -33,8 +42,8 @@ public class StockController extends Controller{
         this.messagesApi = messagesApi;
         
 		this.stocks = new ArrayList<Stock>();
-        stocks.add(new Stock("Apple", "APPL"));
-        stocks.add(new Stock("Google", "GOOG"));
+        //stocks.add(new Stock("APPL", "Apple"));
+        //stocks.add(new Stock("GOOG", "Google"));
         
     }
 
@@ -44,20 +53,78 @@ public class StockController extends Controller{
         return ok(views.html.listStocks.render(asScala(stocks), form, request, messagesApi.preferred(request)));
     }
 
-    public Result addStock(Http.Request request)
+    public Result addStock(Http.Request request) throws IOException
     {
         final Form<Stock> boundForm = form.bindFromRequest(request);
+
+        //listStock listStocks = forms.bindFromRequest(request).get();
 
         if(boundForm.hasErrors()){
             logger.error("error = {}", boundForm.errors());
             return badRequest(views.html.listStocks.render(asScala(stocks), boundForm, request, messagesApi.preferred(request)));
         } else{
             Stock data = boundForm.get();
-            stocks.add(new Stock(data.getName(), data.getSymbol()));
+            String symbolInput = "amzn";
+            //String descriptionInput = data.getDescription;
+            String description = getStockDetails(symbolInput);
+            data.setDescription(description);
+            //stocks.add(new Stock(data.getSymbol, data.getDescription));
             return redirect(routes.StockController.listStocks())
                     .flashing("info", "Stock successfully added!");
         }
     }
 
+    public String getStockDetails(String symbol) throws IOException
+    {
+        String APIurl = "https://api.iextrading.com/1.0/stock/" + symbol + "/company";
+        String charset = "UTF-8";
+
+        URLConnection connection = new URL(APIurl).openConnection();
+        connection.setRequestProperty("Accept-Charset", charset);
+        InputStream response = connection.getInputStream();
+        String textValue = new String();
+        Boolean lineEnd = false;
+        Boolean lineStart = false;
+        ArrayList<String> list = new ArrayList<String>();
+        int charValue = 0;
+        while (charValue != -1) {
+            charValue = response.read();
+            if ((char) charValue == '{') {
+                lineStart = true;
+                lineEnd = false;
+            } else if ((char) charValue == '}') {
+                lineEnd = true;
+                lineStart = false;
+            }
+
+            if (lineStart) {
+                textValue += (char) charValue;
+            } else if (lineEnd) {
+                textValue += (char) charValue;
+                list.add(textValue);
+                textValue = "";
+                lineEnd = false;
+            }
+        }
+        String symbolz = "test";
+        String descriptionz = "test";
+
+        Stock stock = new Stock(symbolz, descriptionz);
+        for (int i = 0; i < list.size(); i++) {
+            JSONObject obj = new JSONObject(list.get(i));
+            stock.setSymbol(obj.getString("symbol"));
+            stock.setCompanyName(obj.getString("companyName"));
+            stock.setExchange(obj.getString("exchange"));
+            stock.setIndustry(obj.getString("industry"));
+            stock.setDescription(obj.getString("description"));
+            //String symbols = stock.setSymbol(obj.getString("symbol"));
+            //String description = stock.setDescription(obj.getString("description"));
+            String descriptions = stock.getDescription();
+            stock.setCEO(obj.getString("CEO"));
+        }
+        String descriptions = stock.getDescription();
+        //return stock;
+        return descriptions;
+    }
 
 }
