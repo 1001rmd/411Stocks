cbf494058045c1259d43051d8c649d86c85562ea
AndrewWitherite <47163001+AndrewWitherite@users.noreply.github.com>
2019-04-07 23:46:37 -0400

    Update Stock.java

--- app/models/Stock.java
+++ app/models/Stock.java
@@ -7,11 +7,19 @@ import io.ebean.Model;
 public class Stock{
     public String symbol;
     public String name;
+    public long stockId;
+    public double askPrice;
+    public double bidPrice;
+    public String companyName;
+    public String exchange;
+    public String industry;
+    public String description;
+    public String CEO;
 
-    public Stock(String name, String symbol)
+    public Stock(String symbol, String description)
     {
-        this.name = name;
         this.symbol = symbol;
+        this.description = description;
     }
 
     public void setName(String name){
@@ -27,4 +35,53 @@ public class Stock{
         return symbol;
     }
 
+
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public String getExchange() {
+        return exchange;
+    }
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+    public String getIndustry() {
+        return industry;
+    }
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCEO() {
+        return CEO;
+    }
+    public void setCEO(String cEO) {
+        CEO = cEO;
+    }
+    public long getStockId() {
+        return stockId;
+    }
+    public void setStockId(long stockId) {
+        this.stockId = stockId;
+    }
+    public double getBidPrice() {
+        return bidPrice;
+    }
+    public void setBidPrice(double bidPrice) {
+        this.bidPrice = bidPrice;
+    }
+    public double getAskPrice() {
+        return askPrice;
+    }
+    public void setAskPrice(double askPrice) {
+        this.askPrice = askPrice;
+    }
 }
